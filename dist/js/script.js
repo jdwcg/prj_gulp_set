02d11e9afd5123dc93239bec5333db2173031df1
jdwcg <bigcloudjo@gmail.com>
2023-07-16 19:43:06 +0900

    JS (어글리파이)까지  세팅

---
+++ dist/js/script.js
@@ -0,0 +1,1 @@
+console.log("hi");let test=document.querySelector("body");console.log(test),test.style.border="1px solid blue";
--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require("gulp");
 const fileinclude = require("gulp-file-include");
 const sass = require("gulp-sass")(require("sass"));
 const browserSync = require("browser-sync").create();
+const uglify = require("gulp-uglify");
 
 // HTML include
 gulp.task("fileinclude", function () {
@@ -30,7 +31,20 @@ gulp.task("style", function () {
     .pipe(browserSync.stream()); // Browsersync로 변경된 스타일을 적용합니다.
 });
 
-// Browsersync 초기화 태스크
+// JS
+gulp.task("minify-js", function () {
+  return gulp
+    .src("src/js/**/*.js")
+    .pipe(
+      uglify().on("error", function (err) {
+        console.error(err.toString()); // 오류 출력
+        this.emit("end"); // 오류를 처리하고 작업 계속 진행
+      })
+    )
+    .pipe(gulp.dest("dist/js"));
+});
+
+// Browsersync 초기화
 gulp.task("serve", function () {
   browserSync.init({
     server: {
@@ -41,11 +55,15 @@ gulp.task("serve", function () {
   // 파일 변경 감지 시 Browsersync로 브라우저를 새로고침합니다.
   gulp.watch("src/scss/*.scss", gulp.series("style"));
   gulp
-    .watch(
-      ["src/html/*.html", "src/html/include/*.html"],
-      gulp.series("fileinclude")
-    )
+    .watch("src/**/*.html", gulp.series("fileinclude"))
+    .on("change", browserSync.reload);
+  gulp
+    .watch("src/js/**/*.js", gulp.series("minify-js"))
     .on("change", browserSync.reload);
 });
 
-gulp.task("default", gulp.parallel("fileinclude", "style", "serve"));
+// 기본(default) Gulp 작업
+gulp.task(
+  "default",
+  gulp.parallel("fileinclude", "style", "minify-js", "serve")
+);
